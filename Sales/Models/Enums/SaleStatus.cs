044772603a7ddae83f8446e67494e5bfc73dacb7
Natal Lima <nataldelima@gmail.com>
2025-09-25 12:42:10 -0400

    Other entities and second migration

---
+++ Sales/Models/Enums/SaleStatus.cs
@@ -0,0 +1,8 @@
+namespace Sales.Models.Enums;
+
+public enum SaleStatus
+{
+    Pending = 0,
+    Billed = 1,
+    Canceled = 2
+}
---
+++ Sales/Models/SalesRecords.cs
@@ -0,0 +1,25 @@
+using Sales.Models.Enums;
+
+namespace Sales.Models;
+
+public class SalesRecords
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public double Amount { get; set; }
+    public SaleStatus Status { get; set; }
+    public Seller Seller { get; set; }
+
+    public SalesRecords()
+    {
+    }
+
+    public SalesRecords(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+    {
+        Id = id;
+        Date = date;
+        Amount = amount;
+        Status = status;
+        Seller = seller;
+    }
+}
