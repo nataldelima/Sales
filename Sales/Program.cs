45aa4ef7c592a7fca074f47815b618b47958c388
Natal Lima <nataldelima@gmail.com>
2025-09-25 13:17:46 -0400

    Seeding Service

--- Sales/Program.cs
+++ Sales/Program.cs
@@ -10,11 +10,30 @@ builder.Services.AddDbContext<SalesContext>(options =>
         throw new InvalidOperationException("Connection string 'SalesContext' not found."),
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesContext"))));
 
-
+builder.Services.AddScoped<SeedingService>();
+    
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Executa o seeding SOMENTE em ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var seedingService = services.GetRequiredService<SeedingService>();
+            seedingService.Seed();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao executar o seeding: {ex.Message}");
+        }
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -30,6 +49,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
