7b962db4657a79474ad20b2cf62a933956c619e5
Natal Lima <nataldelima@gmail.com>
2025-09-27 09:11:43 -0400

    Delete Seller

--- Sales/Controllers/SellersController.cs
+++ Sales/Controllers/SellersController.cs
@@ -38,4 +38,27 @@ public class SellersController : Controller
         _sellerService.Insert(seller);
         return RedirectToAction(nameof(Index));
     }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var obj = _sellerService.FindById(id.Value);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        return View();
+    }
+
+    [HttpPost, AutoValidateAntiforgeryToken]
+    public IActionResult Delete(int id)
+    {
+        _sellerService.Remove(id);
+        return RedirectToAction(nameof(Index));
+    }
 }
